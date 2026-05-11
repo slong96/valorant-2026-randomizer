0b1b78074af059109de4a3885501c3a51b2d81a4
slong96 <sophandalong@hotmail.com>
2026-05-11 16:56:31 -0500

    added environment-based admin URL

--- valorant_stack_project/urls.py
+++ valorant_stack_project/urls.py
@@ -2,11 +2,21 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 handler404 = 'valorant_stack_app.views.custom_404'
 
+# Environment-based admin URL
+if settings.DEBUG:
+    admin_url = 'admin/'
+else:
+    admin_url = os.getenv('ADMIN_URL')
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(admin_url, admin.site.urls),
     path('', include('valorant_stack_app.urls'))
 ]
 
